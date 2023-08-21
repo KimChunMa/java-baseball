f49fd169e360e84aafbed8ae6f2607fa07a020ac
Chunma <acttesty44@naver.com>
2023-08-21 16:41:09 +0900

    add(Number): 유효성 추가, 상수 추가
    숫자의 범위, 숫자크기(사이즈) 상수 추가

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -6,29 +6,51 @@ public class Number {
     private int tends;
     private int ones;
 
+    public static final int NUMBER_MIN = 1;
+    public static final int NUMBER_MAX = 9;
+    public static final int NUMBER_SIZE = 3;
+
     public Number(String number) {
         validation(number);
     }
 
-    public void validation(String number) {
+    private void validation(String number) {
+        validateNumbers(number);
+        toParseInt(number);
+        validateRange();
+        checkNumber();
+    }
+
+    private void toParseInt(String number) {
         try {
-            toParseInt(number);
-            checkNumber();
+            threeDigitNumber = Integer.parseInt(number);
         } catch (Exception e) {
             throw new IllegalStateException("잘못된 입력값 입니다!");
         }
-    }
-
-    public void toParseInt(String number) {
-        threeDigitNumber = Integer.parseInt(number);
         hundreds = threeDigitNumber / 100;
         tends = (threeDigitNumber % 100) / 10;
         ones = (threeDigitNumber % 100) % 10;
     }
 
-    public void checkNumber() {
+    private void checkNumber() {
         if (hundreds == tends || tends == ones || hundreds == ones) {
-            throw new IllegalStateException("2개 이상의 동일한 숫자가 입력되었습니다!");
+            throw new IllegalStateException("서로 다른 세자리수가 아닙니다.");
         }
+    }
+
+    private void validateNumbers(String number) {
+        if (number.length() != NUMBER_SIZE) {
+            throw new IllegalArgumentException("세자리수가 아닙니다.");
+        }
+    }
+
+    private void validateRange() {
+        if (checkRange(hundreds) || checkRange(tends) || checkRange(ones)) {
+            throw new IllegalArgumentException("범위 내 숫자가 아닙니다.");
+        }
+    }
+
+    private boolean checkRange(int number) {
+        return number < NUMBER_MIN || number > NUMBER_MAX;
     }
 }
